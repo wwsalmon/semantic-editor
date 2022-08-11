d77651c5d65f6534bbf148f7d8564a074503c801
Samson Zhang <wwsamson@yahoo.com>
2022-08-11 12:22:18 -0400

    create save flow

--- components/SlateEditor.tsx
+++ components/SlateEditor.tsx
@@ -1,22 +1,23 @@
-import {useMemo} from "react";
+import {Dispatch, SetStateAction, useMemo} from "react";
 import {withHistory} from "slate-history";
-import {Slate, Editable, ReactEditor, withReact} from "slate-react";
+import {Editable, ReactEditor, Slate, withReact} from "slate-react";
 import {createEditor, Descendant} from "slate";
 
-const initialValue: Descendant[] = [
-    {
-        type: "paragraph",
-        children: [
-            {text: "Test"},
-        ]
-    }
-]
+function getText(d: Descendant) {
+    if ("text" in d) return d.text;
+    if ("children" in d) return getText(d.children[0]);
+    return "";
+}
 
-export default function SlateEditor() {
+export default function SlateEditor({value, setValue}: { value: string, setValue: Dispatch<SetStateAction<string>> }) {
     const editor = useMemo(() => withHistory(withReact(createEditor() as ReactEditor)), []);
 
     return (
-        <Slate editor={editor} value={initialValue}>
+        <Slate
+            editor={editor}
+            value={value.split("\n").map(d => ({type: "paragraph", children: [{text: d}]}))}
+            onChange={d => setValue(d.map(x => getText(x)).join("\n"))}
+        >
             <Editable placeholder="Enter your text"/>
         </Slate>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,20 @@ import {useState} from "react";
 
 export default function Home() {
     const [search, setSearch] = useState<string>("");
+    const [isSaved, setIsSaved] = useState<boolean>(false);
+    const [doc, setDoc] = useState<string>("Hello world");
+    const [splitDoc, setSplitDoc] = useState<string[]>([]);
+
+    function onSave() {
+        setSplitDoc(doc.split(/[\n|\.|\?|\!]+\s*/));
+        setIsSaved(true);
+    }
 
     return (
         <div className="mx-auto max-w-3xl px-4 my-8">
             <h1 className="font-bold mb-4">Semantic editor</h1>
-            <div className="flex -mx-2">
-                <div className="w-full px-2">
-                    <div className="p-2 border min-h-[200px]">
-                        <SlateEditor/>
-                    </div>
-                </div>
-                <div className="ml-auto w-64 flex-shrink-0 px-2">
+            {isSaved ? (
+                <>
                     <input
                         type="text"
                         className="p-2 border w-full"
@@ -21,8 +24,23 @@ export default function Home() {
                         value={search}
                         onChange={e => setSearch(e.target.value)}
                     />
-                </div>
-            </div>
+                    <div>
+                        {splitDoc.map((d, i) => (
+                            <p key={i} className="my-6">{d}</p>
+                        ))}
+                    </div>
+                    <button className="bg-black p-1 text-white" onClick={() => setIsSaved(false)}>Edit doc</button>
+                </>
+            ) : (
+                <>
+                    <div className="w-full">
+                        <div className="p-2 border min-h-[200px]">
+                            <SlateEditor value={doc} setValue={setDoc}/>
+                        </div>
+                    </div>
+                    <button className="bg-black p-1 text-white" onClick={onSave }>Save doc</button>
+                </>
+            )}
         </div>
     );
 }
